211361a8c49ed3fb85e6c89a663bec6fc1f9089c
Robaiat Hossain Fardin <148525477+robaiat1819@users.noreply.github.com>
2025-05-03 01:17:09 +0600

    Hackerrank Python

---
+++ Hackerrank Python/Arithmetic_Operators.py
@@ -0,0 +1,9 @@
+# a,b = map(int,input("").split())
+a = int(input(""))
+b = int(input(""))
+sum = a+b
+minus = a-b
+mul = a*b
+print(sum)
+print(minus)
+print(mul)
---
+++ Hackerrank Python/Python_Division.py
@@ -0,0 +1,6 @@
+x = int(input(""))
+y = int(input(""))
+intger_divison = x//y
+decimal_divison = x/y
+print(intger_divison)
+print(f"{decimal_divison:.11f}")
