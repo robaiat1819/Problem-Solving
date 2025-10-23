9ee86e007c06362ef5dca3e1fa0c18024d1b7105
Robaiat Hossain Fardin <148525477+robaiat1819@users.noreply.github.com>
2025-10-23 23:08:53 +0600

    Push Hackerrank C++ series

---
+++ Hackerrank C++/Basic_Data_Types.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    long long int b;
+    char ch;
+    float d;
+    double e;
+    cin >> a >> b >> ch >> d >> e;
+    cout << a << endl
+         << b << endl
+         << ch << endl
+         << fixed << setprecision(3) << d << endl
+         << fixed << setprecision(9) << e << endl;
+    return 0;
+}
---
+++ Hackerrank C++/Input_and_Output.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x, y, z;
+    cin >> x >> y >> z;
+    int restult = x + y + z;
+    cout << restult;
+
+    return 0;
+}
